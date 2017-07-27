dba1ed62de20ab0ff2deb68f604380f9a44695af
Harry Beadle <harry.beadle@gmail.com>
2017-07-27 17:20:20 +0100

    added reset button, tried to fix the offset on the canvas

--- bs-train.js
+++ bs-train.js
@@ -1,15 +1,43 @@
 var c = document.getElementById("bs-train");
 var ctx = c.getContext("2d");
 var img = document.getElementById("wall-img");
-var mode = 'start'
+var mode = 'start';
 
-window.onload = function() {
+function reset() {
   ctx.drawImage(img,0,0,c.width,c.height);
 }
 
+window.onload = function() {
+  reset();
+}
+
+function relMouseCoords(event){
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = this;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+
+    return {x:canvasX, y:canvasY}
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
 c.addEventListener('click', function(event){
-  x = event.pageX - c.offsetLeft;
-  y = event.pageY - c.offsetTop;
+  coords = c.relMouseCoords(event);
+  x = coords.x;
+  y = coords.y - 15;
+  // rect = c.getBoundingClientRect();
+  // x = event.clientX - rect.left;
+  // y = event.clientY - rect.top;
   console.log(x, y);
   ctx.beginPath();
   ctx.arc(x, y, 25, 0, 2 * Math.PI, false);
